9f467e53e94f9c3680393aba2c92fd8487682864
anqi-1 <81287406+anqi-1@users.noreply.github.com>
2021-08-05 17:13:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,52 +26,9 @@ def generate_abstractiveSummary(text):
     return p_summary[0]
 
 
-def generate_extractiveSummary(text):
-    # split into sentences
-    sentences = tokenize.sent_tokenize(text)
-
-    # lower caps
-    lst = [s.lower() for s in sentences]
-
-    # remove non-digit char
-    new_lst = [re.sub('[^A-z]', ' ', s) for s in lst]
-
-    # split list of sentences into list of list of words
-    words = [tokenize.word_tokenize(s) for s in new_lst]
-    # words
-
-    # word_freq
-    word_freq = {}
-    for sent in words:
-        for word in sent:
-            if word in word_freq:
-                word_freq[word] += 1
-            else:
-                word_freq[word] = 1
-
-    # get weight of each sentence
-    sent_weight = []
-    for sent in words:
-        weight = 0
-        for word in sent:
-            weight += word_freq[word]
-        sent_weight.append(weight)
-
-    # argsort, get top two sentences and join tgt
-    indice = (-np.array(sent_weight)).argsort()[:2]
-    empty_str = ""
-    for i in list(indice):
-        empty_str += sentences[i] + " "
-
-    return empty_str
-
 
 @app.route("/")
 def home():
-    def generate():
-        for i in range(10):
-            yield "<br/>"   # notice that we are yielding something as soon as possible
-
     return render_template('index.html')
 
 
@@ -80,10 +37,10 @@ def home():
 def extractive_summary():
 
     text = request.form['input_text']
-    summaryE = generate_extractiveSummary(text)
+    #summaryE = generate_extractiveSummary(text)
     summaryA = generate_abstractiveSummary(text)
 
-    return render_template('index.html', title="Extractive Summarizer", original_text=text, output_summary1=summaryE, output_summary2=summaryA)
+    return render_template('index.html', title="Extractive Summarizer", original_text=text, output_summary2=summaryA)
 
 
 if __name__ == "__main__":
